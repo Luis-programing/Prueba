096b1053a2079de8379610f635e8ff5063d1f1f9
Luis Angel Feliz German <113213808+Luis-programing@users.noreply.github.com>
2022-09-10 09:58:34 -0400

    Add files via upload

---
+++ demostracion/script/main.js
@@ -0,0 +1,14 @@
+function principal() {
+    window.location="mensaje.html";
+}
+
+function login(){
+
+}
+
+function btn(){
+    window.location="index.html";
+}
+function msg(){
+    window.location="mensaje.html"
+}
